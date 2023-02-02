43b23cac2851e3e7f83e94564e88efd723ff6fb9
zeljka <ivezazeljka@gmail.com>
2023-02-02 23:53:41 +0100

    some bigger changes to javascript

--- country.js
+++ country.js
@@ -1,176 +1,120 @@
-window.addEventListener("load", fetchData)
-const body = document.querySelector("body")
-const header = document.querySelector("header")
-const title = document.querySelector(".title")
-const switchTheme = document.querySelector(".switchThemeSection")
-const switchLogo = document.querySelector(".switchLogo")
-const themeBtn = document.querySelector(".themeBtn")
-const flag = document.querySelector(".flag")
-const name = document.querySelector(".name")
-const nativeName = document.querySelector(".nativeName")
-const population = document.querySelector(".population")
-const subRegion = document.querySelector(".subRegion")
-const capital = document.querySelector(".capital")
-const domain = document.querySelector(".domain")
-const currencies = document.querySelector(".currencies")
-const languages = document.querySelector(".languages")
-const main = document.querySelector("main")
-const countryButtons = document.querySelector(".countryButtons")
-const countyContainer = document.querySelector(".countryContainer")
-const backButton = document.querySelector(".backButton")
-let light;
+let body = document.querySelector(".body")
+let toggleThemeBtn = document.querySelector(".header__toggle-theme-btn")
+let theme;
+let data;
+let countryContainer = document.querySelector(".main__country-container")
+let borders = []
 
-if(localStorage.light === undefined){
-    localStorage.setItem("light", true)
-    light = JSON.parse(localStorage.light)
-}else {
-    light = JSON.parse(localStorage.light)    
-}
-
-if(light === false){
-    toggleTheme()
-}
-
-
-function fetchData(){
-    let alpha = localStorage.getItem("alphaCode")
-    alpha = alpha.toLowerCase()
-
-    fetch(`https://restcountries.com/v2/alpha/${alpha}`)
-    .then((res) => res.json())
-    .then((res) => createCards(res))
-}
-
-function createCards(data){
-    let country = data
-
-    flag.setAttribute("src", country.flags.svg)
-    name.innerText = country.name
-
-    let nativeNameInfo = document.createElement("span")
-    let populationInfo = document.createElement("span")
-    let subRegionInfo = document.createElement("span")
-    let capitalInfo = document.createElement("span")
-    let domainInfo = document.createElement("span")
-    let currenciesInfo = document.createElement("span")
-    let languagesInfo = document.createElement("span")
-
-    let arrLang = []
-    country.languages.forEach(e => arrLang.push(e.name))
-
-    nativeNameInfo.innerText = country.nativeName
-    populationInfo.innerText = Number(country.population).toLocaleString() 
-    subRegionInfo.innerText = country.subregion
-    if(country.capital != undefined) capitalInfo.innerText = country.capital
-    else capitalInfo.innerText = "N/A"
-    domainInfo.innerText = country.topLevelDomain[0]
-    if(country.currencies != undefined){
-        currenciesInfo.innerText = country.currencies[0].name
-    }else currenciesInfo.innerText = "N/A"
-    languagesInfo.innerText = arrLang.join()
-    nativeNameInfo.classList.add("nonBold")
-    populationInfo.classList.add("nonBold")
-    subRegionInfo.classList.add("nonBold")
-    capitalInfo.classList.add("nonBold")
-    domainInfo.classList.add("nonBold")
-    currenciesInfo.classList.add("nonBold")
-    languagesInfo.classList.add("nonBold")
-
-    // border countries
-    if (country.borders != undefined){
-        country.borders.forEach((e, i) => {
-            let alpha = e.toLowerCase()
-    
-            fetch(`https://restcountries.com/v2/alpha/${alpha}`)
-            .then((res) => res.json())
-            .then((res) => createBorder(res))
-    
-            function createBorder (borderCountry){
-                let button = document.createElement("button")
-                let snippet = document.createElement("div")
-    
-                button.classList.add("borderCountry")
-                snippet.classList.add("snippet")
-                button.innerText = borderCountry.alpha3Code
-                snippet.innerText = borderCountry.name
-                snippet.id = borderCountry.name
-                button.addEventListener("click", goToBorderCountry)
-                button.addEventListener("mouseover", ()=>{
-                    snippet.style.display = "block"
-                })
-                button.addEventListener("mouseleave", ()=>{
-                    snippet.style.display = "none"
-                })
-
-                function goToBorderCountry (){
-                    localStorage.setItem("alphaCode", borderCountry.alpha3Code)
-                    window.location.reload()
-                }
-    
-                countryButtons.append(button)
-                button.append(snippet)
-
-                if (light === true){
-                    button.classList.remove("borderCountry-darkMode")
-                    snippet.classList.remove("snippet-darkMode")
-                }else{
-                    button.classList.add("borderCountry-darkMode")
-                    snippet.classList.add("snippet-darkMode")
-                }
-                
-                if(i == country.borders.length - 1){
-                    main.style.visibility = "visible"
-                }
-            }
-        })
-    }else{
-        countryButtons.innerText = "No border countries"
+//setting the theme value on local storage
+if(localStorage.getItem("theme")){
+    theme = JSON.parse(localStorage.getItem("theme"))
+    if(theme === false){
+        body.classList.add("body--dark-theme")
+        changeThemeBtnAppearance()
     }
-
-    nativeName.append(nativeNameInfo)
-    population.append(populationInfo)
-    subRegion.append(subRegionInfo)
-    capital.append(capitalInfo)
-    domain.append(domainInfo)
-    currencies.append(currenciesInfo)
-    languages.append(languagesInfo)
-    
-    main.style.visibility = "visible"
-    
+}else{
+    localStorage.setItem("theme", true) 
 }
 
-switchTheme.addEventListener("click", ()=>{
-    light = !light
-    localStorage.setItem("light", light)
-    toggleTheme()
+window.addEventListener("load", ()=>{
+    fetchCountry(localStorage.getItem("transferred"))
+    setTimeout(()=>{
+        createCountry(data)
+    }, 500)
 })
 
-function toggleTheme(){
-    body.classList.toggle("body-darkMode")
-    header.classList.toggle("header-darkMode")
-    title.classList.toggle("title-darkMode")
-    themeBtn.classList.toggle("themeBtn-darkMode")
-    countyContainer.classList.toggle("countryContainer-darkMode")
-    backButton.classList.toggle("backButton-darkMode")
+function fetchCountry (keyword){
+    fetch(`https://restcountries.com/v3.1/alpha/${keyword}`)
+    .then(res => res.json())
+    .then(res => {
+        data = res[0]
+        if(res[0].borders){
+            res[0].borders.forEach(e =>{
+                fetchBorderCountries(e)
+            })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
 
-    if(light == true){
-        switchLogo.setAttribute("src", "./Assets/moon-outline.svg")
-        themeBtn.innerText = "Dark mode"
-    }else{
-        switchLogo.setAttribute("src", "./Assets/icon-sun.svg")
-        themeBtn.innerText = "Light mode"
+function fetchBorderCountries(keyword){
+    fetch(`https://restcountries.com/v3.1/alpha/${keyword}`)
+    .then(res => res.json())
+    .then(res => {
+        borders.push(res[0])
+    })
+    .catch(error => console.log(error))
+}
+
+function createCountry(data){
+    if(data.languages){
+        var languages = Object.values(data.languages).join(", ")
+    }
+    if(data.name.nativeName){
+        var nativeName = Object.values(data.name.nativeName)
+    }
+    if(data.currencies){
+        var currencies = Object.values(data.currencies)
     }
 
-    let borderCountries = document.querySelectorAll(".borderCountry")
-    if(borderCountries.length > 0){
-        borderCountries.forEach(e => {
-            if(light === true) e.classList.remove("borderCountry-darkMode")
-            else e.classList.add("borderCountry-darkMode")
+    countryContainer.innerHTML = `
+        <img class="main__flag" src="${data.flags.svg}"/>
+        <div class="main__info-container">
+            <h2 class="main__name">${data.name.common}</h2>
+            <div class="main__sub-info-container">
+                <div class="main__information-first-part">
+                    <p>Native Name:  <span>${nativeName ? nativeName[0].common : "N/A"}</span></p>
+                    <p>Population:  <span>${data.population.toLocaleString()}</span></p>
+                    <p>Region:  <span>${data.region ? data.region : "N/A"}</span></p>
+                    <p>Sub Region:  <span>${data.subregion ? data.subregion : "N/A"}</span></p>
+                    <p>Capital:  <span>${data.capital ? data.capital[0] : "N/A"}</span></p>
+                </div>
+                <div class="main__information-second-part">
+                    <p>Top Level Domain:  <span>${data.tld ? data.tld[0] : "N/A"}</span></p>
+                    <p>Currencies:  <span>${currencies ? currencies[0].name : "N/A"}</span></p>
+                    <p>Languages:  <span>${languages ? languages : "N/A"}</span></p>
+                </div>
+            </div>
+            <div class="main__borders-container">
+                <p class="main__borders-title">Borders:</p>
+                <div class="main__borders-items"></div>
+            </div>
+        </div>
+    `;
+
+    if(borders.length){
+        borders.forEach((e, i) => {    
+            let borderItem = document.createElement("span")
+            borderItem.innerText = `${e.cca3}`
+            borderItem.classList.add("main__border-item")
+            document.querySelector(".main__borders-items").appendChild(borderItem)
+    
+            borderItem.addEventListener("click", ()=>{
+                localStorage.setItem("transferred", e.cca3)
+                window.location.reload()
+            })
         })
-        let snippets = document.querySelectorAll(".snippet")
-        snippets.forEach(e => {
-            if(light === true) e.classList.remove("snippet-darkMode")
-            else e.classList.add("snippet-darkMode")
-        })
+    }else{
+        document.querySelector(".main__borders-items").innerHTML = `<span>No bordering countries</span>`
     }
 }
+
+function changeTheme(){
+    theme = !theme
+    localStorage.setItem("theme", theme)
+    body.classList.toggle("body--dark-theme")
+    changeThemeBtnAppearance()
+}
+
+function changeThemeBtnAppearance(){
+    let img = document.querySelector(".header__toggle-theme-img");
+    let text = document.querySelector(".header__toggle-theme-btn-text")
+    theme ? img.src = "./Assets/moon.svg" : img.src = "./Assets/sun.svg"
+    theme ? text.innerText = "Dark mode" : text.innerText = "Light mode" 
+}
+
+toggleThemeBtn.addEventListener("click", ()=>{
+    changeTheme()
+})
